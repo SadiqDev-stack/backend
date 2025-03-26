57d1b99a3d50d956942aee06bd174977d2cddbf8
Sadiq <sadiqmuh1321@gmail.com>
2025-03-26 22:49:14 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./statics"));
+
+
+app.get('/',(req,res) => {
+  res.json({
+    message: "welcome to backend"
+  })
+})
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log("server started at " + port);
+});
