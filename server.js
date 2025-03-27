2f9c56bd97eea2bb15bbf9c4f12ae9d880045c91
Sadiq <sadiqmuh1321@gmail.com>
2025-03-27 12:03:01 +0100

    server.js

--- server.js
+++ server.js
@@ -59,26 +59,4 @@ app.delete('/posts/:id', (req, res) => {
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
-```const express = require("express");
-const app = express();
 
-app.use(express.static("./statics"));
-
-
-app.get('/',(req,res) => {
-  res.json({
-    message: "welcome to backend"
-  })
-})
-
-const port = process.env.PORT;
-app.listen(port, () => {
-    console.log("server started at " + port);
-});
-
-app.get('/api',(req,res) => {
- res.json({
-   message: "welcome to api"
-})
-})
- 
